b2d09698ecf10e7d7cda35d1dcb83df3808e27a4
vinlearngit <vinlearngit@gmail.com>
2025-07-02 11:51:02 +0530

    Delete Program.cs

--- Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello from .NET + Azure!");
-app.MapGet("/health", () => Results.Ok("Healthy"));
-
-app.Run();
